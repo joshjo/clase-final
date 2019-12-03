9cb10d561dcf306748354a692bbd751c7596e61e
Josué Ttito <josue.ttito@ucsp.edu.pe>
2019-12-03 16:37:27 -0500

    se agrego el archivo timer

--- src/counter.js
+++ src/counter.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import {
   View,
+  Text,
+  StyleSheet,
 } from 'react-native';
+import CircleButton from './circle-button';
+import { pad } from './utils';
+
 
 function Counter ({ maximum, minimum, initial, onChange }) {
   const [value, setValue] = React.useState(initial);
 
   function decrement() {
     if (value > minimum) {
+      const newValue = value - 1;
+      onChange(newValue);
+      setValue(newValue);
     }
   }
 
   function increment() {
     if (value < maximum) {
+      const newValue = value + 1;
+      onChange(newValue);
+      setValue(newValue);
     }
   }
 
@@ -22,6 +33,13 @@ function Counter ({ maximum, minimum, initial, onChange }) {
       justifyContent: 'center',
       alignItems: 'center',
     }}>
+      <CircleButton onPress={increment} icon="plus" color="#C7F464" iconColor="#556270" />
+      <Text style={{
+        fontSize: 48,
+        color: '#556270',
+        marginVertical: 15,
+      }}>{pad(value, 2)}</Text>
+      <CircleButton onPress={decrement} icon="minus" color="#C7F464" iconColor="#556270" />
     </View>
   );
 }
--- src/timer.js
+++ src/timer.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import { View } from 'react-native';
-
+import { View, Text, Button, TouchableOpacity } from 'react-native';
+import Counter from './counter';
+import { formatTime } from './utils';
+import CircleButton from './circle-button';
 
 
 class Timer extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.interval = null;
+    this.state = {
+      clock: 0,
+      minutes: 0,
+      seconds: 0,
+    };
   }
 
   start = () => {
+    const { minutes, seconds } = this.state;
+
+    const totalSeconds = minutes * 60 + seconds;
+
+    this.setState({ clock: totalSeconds }, () => {
+      this.interval = setInterval(() => {
+        this.setState({ clock: this.state.clock - 1 });
+      }, 1000);
+    });
   }
 
   stop = () => {
+    this.setState({ clock: 0 });
+    clearInterval(this.interval);
   }
 
   componentDidUpdate() {
+    if (this.state.clock === 0) {
+      clearInterval(this.interval);
+    }
   }
 
   render() {
+    const { clock, minutes, seconds } = this.state;
+
     return (
       <View
         style={{
@@ -28,6 +49,39 @@ class Timer extends React.PureComponent {
           alignItems: 'center',
         }}
       >
+        <View style={{
+          flexDirection: 'row',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <Counter
+            minimum={0}
+            maximum={60}
+            initial={minutes}
+            onChange={(minutes) => this.setState({ minutes })}
+          />
+          <Counter
+            minimum={0}
+            maximum={60}
+            initial={seconds}
+            onChange={(seconds) => this.setState({ seconds })}
+          />
+        </View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          { clock > 0 ? (
+            <CircleButton onPress={this.stop} size={100} icon="stop" iconColor="white" color="#C44D58" />
+          ) : (
+            <CircleButton onPress={this.start} size={100} icon="play" iconColor="white" color="#556270" />
+          ) }
+          <Text
+            style={{
+              marginVertical: 16,
+              fontSize: 56,
+              color: '#556270',
+            }}
+          >{formatTime(clock)}</Text>
+        </View>
       </View>
     );
   }
